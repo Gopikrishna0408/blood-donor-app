90c717ee08914f4be002735cdf0ee3159c5b7a7a
Gopikrishna0408 <120036421+Gopikrishna0408@users.noreply.github.com>
2026-02-18 14:37:12 +0530

    Update CustomerUserDetailsService.java

--- blooddonor/src/main/java/com/bloodfinder/blood_donor_backend/config/CustomerUserDetailsService.java
+++ blooddonor/src/main/java/com/bloodfinder/blood_donor_backend/config/CustomerUserDetailsService.java
@@ -1,15 +1,16 @@
 package com.bloodfinder.blood_donor_backend.config;
 
-import java.util.Optional;
 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.bloodfinder.blood_donor_backend.entity.Donor;
 import com.bloodfinder.blood_donor_backend.repository.DonorRepository;
 
+@Service
 public class CustomerUserDetailsService implements UserDetailsService{
 	
 	private DonorRepository donorRepository;
